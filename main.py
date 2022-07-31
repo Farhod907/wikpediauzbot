162970e5d99a014800c75173b11ef0096db430e8
Farkhod.Fd <108648883+Farhod907@users.noreply.github.com>
2022-07-31 13:50:19 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+import logging
+import wikipedia
+
+from aiogram import Bot, Dispatcher, executor, types
+API_TOKEN = '5470198871:AAENlaFtw9R3imSwPKxu8Mn13lMfIt6HUuY'
+wikipedia.set_lang('uz')
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    await message.reply("Assalomu Alaykum Wikipedia botda Xush kelibsiz\n Istalgan mavzuda malumot axtarishingiz mumkin")
+@dp.message_handler(commands=[ 'help'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    await message.reply("Tez orada siz bilan Aperatorlarimiz bolg'lanishadi\n Iltimos kuting")
+
+@dp.message_handler()
+async def sendwiki(message: types.Message):
+    try:
+        respond = wikipedia.summary(message.text)
+        await  message.answer(respond)
+    except:
+        await message.answer("bu mavzuga oid malumot topilmadi \n boshqa nom bilan qidiring")
+
+
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
